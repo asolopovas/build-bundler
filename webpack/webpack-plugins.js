839c375d7c0d2d978c7f4b2517b620425abfd5f0
Andrius Solopovas <andrius.solopovas@gmail.com>
2020-06-24 18:31:59 +0100

    save

--- webpack/webpack-plugins.js
+++ webpack/webpack-plugins.js
@@ -1,13 +1,15 @@
 const argv = require('yargs').argv
 const webpack = require('webpack')
 const ProductionLogic = require('./plugins/production-logic')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const Dotenv = require('dotenv-webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const Dotenv = require('dotenv-webpack')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const dev = require('../src/config')
 
 const plugins = [
   new Dotenv(),
+  new VueLoaderPlugin()
 ]
 
 if ( dev.js.opts.hasOwnProperty('plugins') ) {

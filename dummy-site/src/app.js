e6f19757b14445b45fa09de2c3400bd480834dac
Andrius Solopovas <andrius.solopovas@gmail.com>
2020-10-06 21:48:53 +0100

    save

--- dummy-site/src/app.js
+++ dummy-site/src/app.js
@@ -1,2 +1,1 @@
-console.log('test')
-$('.book')
+import Vue from 'vue'
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
+const {parallel} = require('gulp')
 const tasks = require('./gulp-tasks')
+const nodemon = require('gulp-nodemon')
 
 function CleanExistingAssets(done) {
   tasks.Clean([dev.sass.dest, dev.js.dest])
   done()
 }
 
+function server(done) {
+  nodemon({
+    script: './src/server/server',
+    ext: 'js html',
+    env: {'NODE_ENV': 'development'},
+    done: done,
+  })
+}
+
 exports.clean = CleanExistingAssets
 exports.sass = tasks.Sass
+exports.server = parallel(server, tasks.BrowserSync)
+
 exports.browserSync = tasks.BrowserSync
 exports.webpack = tasks.Webpack
 exports.serviceWorker = tasks.ServiceWorker

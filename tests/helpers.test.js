d3ad72db60b77d99c5e3e67ce406810681075816
Andrius Solopovas <andrius.solopovas@gmail.com>
2020-10-06 13:33:20 +0100

    updated all modules

--- tests/helpers.test.js
+++ tests/helpers.test.js
@@ -5,15 +5,12 @@ import {publicPath, absolutePath} from '../src/helpers'
 let rootPath = 'home/site.test/wp-content/themes/test_theme/'
 
 test('returns correct public path', t => {
-
   t.is(publicPath('static/src/index.js', rootPath), '/wp-content/themes/test_theme/static/src/index.js')
   t.is(publicPath('static/src', rootPath), '/wp-content/themes/test_theme/static/src')
   t.is(publicPath('static/src/', rootPath), '/wp-content/themes/test_theme/static/src')
-
 })
 
 test('return correct absolute path', t => {
-
   const paths = ['static/src/index1.js', 'static/src/index2.js']
 
   t.is(absolutePath('static/src/index.js', rootPath), path.resolve(rootPath, 'static/src/index.js'))
